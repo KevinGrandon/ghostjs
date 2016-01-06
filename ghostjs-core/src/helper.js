72a4ca33328648821cb5a00b8c9b53a27a08aa96
Kevin Grandon <kevingrandon@yahoo.com>
2016-01-06 15:40:22 -0800

    Move babel registration into mocha helper.

--- ghostjs-core/src/helper.js
+++ ghostjs-core/src/helper.js
@@ -1,1 +1,5 @@
-import 'babel-polyfill'
+require('babel-polyfill')
+require('babel-core/register')({
+    // Ignore everything in node_modules except node_modules/ghostjs.
+    ignore: /node_modules\/(?!ghostjs)/
+})
