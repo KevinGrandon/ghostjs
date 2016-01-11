7e10f598b8ccd51a993474660a9d2f5edfa6b754
Kevin Grandon <kevingrandon@yahoo.com>
2016-01-10 22:52:41 -0800

    Add a test for a mousedown event.

---
+++ ghostjs-examples/test/mouse_events_test.js
@@ -0,0 +1,20 @@
+import ghost from 'ghostjs'
+import assert from 'assert'
+
+import localServer from './fixtures/server.js'
+
+describe('element mouse events test', () => {
+
+  before(localServer)
+
+  it('triggers mousedown event', async () => {
+    await ghost.open('http://localhost:8888/mouse_test.html')
+
+    let target = await ghost.findElement('#target')
+    assert.equal(await target.text(), 'default')
+
+    await target.mouse('mousedown')
+
+    assert.equal(await target.text(), 'mousedown')
+  })
+})
