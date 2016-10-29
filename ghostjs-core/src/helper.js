6f3e0b12cdf2c85eb5fcc020af78561ebec97a2a
Christian Vuerings <cvuerings@pinterest.com>
2016-10-28 17:51:50 -0700

    Make sure we include ghost-axe in the babel transforms

--- ghostjs-core/src/helper.js
+++ ghostjs-core/src/helper.js
@@ -1,5 +1,5 @@
 require('babel-polyfill')
 require('babel-core/register')({
   // Ignore everything in node_modules except node_modules/ghostjs.
-  ignore: /node_modules\/(?!ghostjs)/
+  ignore: /node_modules\/(?!ghost*)/
 })
